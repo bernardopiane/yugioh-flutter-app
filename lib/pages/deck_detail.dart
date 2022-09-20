ecfb634c8cfec5fcbbbda25e17fe834ba47ad01b
bernardopiane <bernardopiane@gmail.com>
2022-09-20 11:51:41 -0300

    Removed unused imports

--- lib/pages/deck_detail.dart
+++ lib/pages/deck_detail.dart
@@ -1,21 +1,14 @@
 import 'dart:collection';
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:yugi_deck/card_info_entity.dart';
-import 'package:yugi_deck/globals.dart';
 import 'package:yugi_deck/models/deck.dart';
 import 'package:yugi_deck/models/deck_list.dart';
 import 'package:yugi_deck/utils.dart';
 import 'package:yugi_deck/variables.dart';
-import 'package:yugi_deck/widgets/card_grid_view.dart';
 import 'package:yugi_deck/widgets/card_width_slider.dart';
 import 'package:yugi_deck/widgets/my_card.dart';
-
-import 'package:popover/popover.dart';
 
 class DeckDetail extends StatefulWidget {
   const DeckDetail({Key? key, required this.deck}) : super(key: key);
