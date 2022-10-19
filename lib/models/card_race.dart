449577b87abe2d1f937bfe3e25a98ea9cef790ce
bernardopiane <bernardopiane@gmail.com>
2022-10-19 14:49:45 -0300

    Added new models

---
+++ lib/models/card_race.dart
@@ -0,0 +1,42 @@
+List<String> monsterRaceList = [
+  "Aqua",
+  "Beast",
+  "Beast-Warrior",
+  "Creator-God",
+  "Cyberse",
+  "Dinosaur",
+  "Divine-Beast",
+  "Dragon",
+  "Fairy",
+  "Fiend",
+  "Fish",
+  "Insect",
+  "Machine",
+  "Plant",
+  "Psychic",
+  "Pyro",
+  "Reptile",
+  "Rock",
+  "Sea Serpent",
+  "Spellcaster",
+  "Thunder",
+  "Warrior",
+  "Winged Beast",
+  "Wyrm",
+  "Zombie",
+];
+
+List<String> spellRaceList = [
+  "Normal",
+  "Field",
+  "Equip",
+  "Continuous",
+  "Quick-Play",
+  "Ritual",
+];
+
+List<String> trapRaceList = [
+  "Normal",
+  "Continuous",
+  "Counter",
+];
