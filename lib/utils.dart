c5204c0c96a83b473cb9285d6b419cdac29af5ec
bernardopiane <bernardopiane@gmail.com>
2023-04-17 13:29:09 -0300

    improved error handling & user feedback

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:yugi_deck/globals.dart';
 import 'package:yugi_deck/models/card_v2.dart';
@@ -6,64 +9,113 @@ import 'card_info_entity.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future<List<CardV2>> fetchCardList(
-    String url, BuildContext context) async {
-  var response = await http.post(Uri.parse(url));
+// Future<List<CardV2>> fetchCardList(
+//     String url, BuildContext context) async {
+//   var response = await http.post(Uri.parse(url));
+//
+//   List<CardInfoEntity> cardList = [];
+//   List<CardV2> cardV2List = [];
+//
+//   var json = jsonDecode(response.body);
+//
+//   if (json["error"] != null) {
+//     var snackBar = SnackBar(
+//       content: Text(json["error"].toString()),
+//     );
+//     snackbarKey.currentState?.showSnackBar(snackBar);
+//
+//     // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+//     return cardV2List;
+//   }
+//
+//   var lista = json["data"] as List;
+//
+//
+//   for (var element in lista) {
+//     CardInfoEntity cardInfo = CardInfoEntity.fromJson(element);
+//     CardV2 card = CardV2.fromJson(element);
+//     cardV2List.add(card);
+//     cardList.add(cardInfo);
+//   }
+//
+//   debugPrint(cardV2List.elementAt(0).name);
+//
+//   return cardV2List;
+// }
 
-  List<CardInfoEntity> cardList = [];
-  List<CardV2> cardV2List = [];
+Future<List<CardV2>> fetchCardList(String url, BuildContext context) async {
+  try {
+    var response =
+        await http.post(Uri.parse(url)).timeout(const Duration(seconds: 30));
 
-  var json = jsonDecode(response.body);
+    List<CardInfoEntity> cardList = [];
+    List<CardV2> cardV2List = [];
 
-  if (json["error"] != null) {
-    var snackBar = SnackBar(
-      content: Text(json["error"].toString()),
-    );
-    snackbarKey.currentState?.showSnackBar(snackBar);
+    var json = jsonDecode(response.body);
 
-    // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    if (json["error"] != null) {
+      var snackBar = SnackBar(
+        content: Text(json["error"].toString()),
+      );
+      snackbarKey.currentState?.showSnackBar(snackBar);
+
+      return cardV2List;
+    }
+
+    var lista = json["data"] as List;
+
+    for (var element in lista) {
+      CardInfoEntity cardInfo = CardInfoEntity.fromJson(element);
+      CardV2 card = CardV2.fromJson(element);
+      cardV2List.add(card);
+      cardList.add(cardInfo);
+    }
+
+    debugPrint(cardV2List.elementAt(0).name);
+
     return cardV2List;
+  } on TimeoutException catch (_) {
+    var snackBar = const SnackBar(
+      content:
+          Text("Request timed out. Please check your internet connection."),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    return [];
+  } on SocketException catch (_) {
+    var snackBar = const SnackBar(
+      content: Text('No internet connection'),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    return [];
+  } catch (e) {
+    var snackBar = const SnackBar(
+      content: Text("An error occurred. Please try again later."),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    return [];
   }
-
-  var lista = json["data"] as List;
-
-
-  for (var element in lista) {
-    CardInfoEntity cardInfo = CardInfoEntity.fromJson(element);
-    CardV2 card = CardV2.fromJson(element);
-    cardV2List.add(card);
-    cardList.add(cardInfo);
-  }
-
-  debugPrint(cardV2List.elementAt(0).name);
-
-  return cardV2List;
 }
 
 bool isExtraDeck(CardV2 card) {
-  if ((card.type!.contains("Fusion Monster") ||
+  if (card.type!.contains("Fusion Monster") ||
       card.type!.contains("Link Monster") ||
       card.type!.contains("Pendulum Effect Fusion Monster") ||
       card.type!.contains("Synchro Monster") ||
       card.type!.contains("Synchro Pendulum Effect Monster") ||
       card.type!.contains("Synchro Tuner Monster") ||
       card.type!.contains("XYZ Monster") ||
-      card.type!.contains("XYZ Pendulum Effect Monster"))) {
+      card.type!.contains("XYZ Pendulum Effect Monster")) {
     return true;
   }
   return false;
 }
 
-
-bool isBelowDeckLimit(List<CardV2> cards, bool isExtraDeck){
-  if(isExtraDeck){
-    if(cards.length < 15){
-      return true;
-    }
-  } else {
-    if(cards.length < 60){
-      return true;
-    }
+bool isBelowDeckLimit(List<CardV2> cards, bool isExtraDeck) {
+  if (isExtraDeck && cards.length < 15) {
+    return true;
+  }
+  if (!isExtraDeck && cards.length < 60) {
+    return true;
   }
   return false;
 }
