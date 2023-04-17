e890e05ae086067704c82ff6ad22f5c57ebc08bb
bernardopiane <bernardopiane@gmail.com>
2023-04-17 13:28:25 -0300

    implemented search class

---
+++ lib/widgets/filter_attributes.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class FilterAttributes extends StatefulWidget {
+  const FilterAttributes({Key? key}) : super(key: key);
+
+  @override
+  _FilterAttributesState createState() => _FilterAttributesState();
+}
+
+class _FilterAttributesState extends State<FilterAttributes> {
+  final List<String> cardAttributes = [
+    'LIGHT',
+    'DARK',
+    'WATER',
+    'FIRE',
+    'EARTH',
+    'WIND',
+    'DIVINE',
+  ];
+
+  Set<String> selectedCardTypes = {};
+
+  void toggleCardType(String cardType) {
+    setState(() {
+      if (selectedCardTypes.contains(cardType)) {
+        selectedCardTypes.remove(cardType);
+      } else {
+        selectedCardTypes.add(cardType);
+      }
+    });
+  }
+
+  bool isCardTypeSelected(String cardType) {
+    return selectedCardTypes.contains(cardType);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 8.0,
+      children: cardAttributes.map((cardType) {
+        return FilterChip(
+          showCheckmark: false,
+          selectedColor: Colors.amber,
+          //TODO follow games colors pattern
+          label: Text(cardType),
+          selected: isCardTypeSelected(cardType),
+          onSelected: (isSelected) {
+            toggleCardType(cardType);
+          },
+        );
+      }).toList(),
+    );
+  }
+}
---
+++ lib/widgets/filter_types.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class FilterTypes extends StatefulWidget {
+  const FilterTypes({Key? key}) : super(key: key);
+
+  @override
+  _FilterTypesState createState() => _FilterTypesState();
+}
+
+class _FilterTypesState extends State<FilterTypes> {
+  final List<String> cardTypes = [
+    'Normal',
+    'Effect',
+    'Fusion',
+    'Ritual',
+    'Synchro',
+    'Xyz',
+    'Pendulum',
+    'Link',
+    'Spell',
+    'Trap',
+  ];
+
+  Set<String> selectedCardTypes = {};
+
+  void toggleCardType(String cardType) {
+    setState(() {
+      if (selectedCardTypes.contains(cardType)) {
+        selectedCardTypes.remove(cardType);
+      } else {
+        selectedCardTypes.add(cardType);
+      }
+    });
+  }
+
+  bool isCardTypeSelected(String cardType) {
+    return selectedCardTypes.contains(cardType);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 8.0,
+      runSpacing: 8.0,
+      children: cardTypes.map((cardType) {
+        return FilterChip(
+          showCheckmark: false,
+          selectedColor: Colors.amber,
+          //TODO follow games colors pattern
+          label: Text(cardType),
+          selected: isCardTypeSelected(cardType),
+          onSelected: (isSelected) {
+            toggleCardType(cardType);
+          },
+        );
+      }).toList(),
+    );
+  }
+}
